880ae698a5b0843b30be64b46904a638190c4a66
Donald Pakkies <donaldpakkies@gmail.com>
2017-01-20 03:46:32 +0200

    Add files via upload

---
+++ Hello World/app.js
@@ -0,0 +1,26 @@
+﻿/* App Code */
+
+    function Hello_World_Load() {
+        document.title                 = 'Hello World';
+        document.body.style.background = '#eee';
+        document.body.style.color      = 'black';
+        document.body.style.margin     = '0px';
+        helloworld();
+        application.ready('show');
+    }
+    
+    function application_onReady() {
+        application.onExit('kill');
+    }
+    
+    function helloworld() {
+        inputDialog.show('What is your name?', '', 'OK');
+        inputDialog.catchresults();
+        
+        var name = inputDialog.inputdata;
+        if (name != '')
+        {
+            output.writeline('Hello '+ name);
+            document.getElementById('lblh').innerHTML = 'Hello '+ name;
+        }
+    }
